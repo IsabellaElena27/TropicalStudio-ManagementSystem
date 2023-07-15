ee74597a854935a1717b4b553a7f5ebf31af9908
IsabellaElena27 <ysss_a@yahoo.com>
2023-07-16 00:09:00 +0300

    changes

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -20,14 +20,6 @@ window.addEventListener('DOMContentLoaded', event => {
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
 
-    // // Activate Bootstrap scrollspy on the main nav element
-    // const mainNav = document.body.querySelector('#mainNav');
-    // if (mainNav) {
-    //     new bootstrap.ScrollSpy(document.body, {
-    //         target: '#mainNav',
-    //         rootMargin: '0px 0px -40%',
-    //     });
-    // };
 
     const scrollSpy = new bootstrap.ScrollSpy(document.body, {
         target: '#mainNav',
@@ -55,11 +47,5 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
-
-
-    // // Activate SimpleLightbox plugin for portfolio items
-    // new SimpleLightbox({
-    //     elements: '#portfolio a.portfolio-box'
-    // });
 
 });
