5ab28720e7a04b2130b9983b1261ddca93956132
IsabellaElena27 <ysss_a@yahoo.com>
2023-07-25 18:01:03 +0300

    changes services/views

--- services/views.py
+++ services/views.py
@@ -1,13 +1,9 @@
-from datetime import datetime
-
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView, TemplateView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
 
-import employee
 from employee.models import Employee
 from services.forms import ServiceForm, ServiceUpdateForm, SelectareDataOraForm
 from services.models import Service, RezervareServicii
@@ -53,44 +49,52 @@ class ServiceDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView)
     permission_required = 'services.view_service'
 
 
-class Rezervare(ListView):
-    template_name = 'services/rezervare.html'
-    model = Service
-    context_object_name = 'all_services'
-    success_url = reverse_lazy('homepage')
+class Rezervare(ListView):  # afiseaza o lista de obiecte dintr-un model specific
+    template_name = 'services/rezervare.html'  # specifică șablonul HTML care va fi folosit pentru a afișa rezultatele.
+    model = Service  # Specifică modelul pe care îl va utiliza vizualizarea pentru a obține datele.
+    context_object_name = 'all_services'  # Definește numele sub care obiectele din modelul specificat vor fi disponibile în contextul șablonului.
+    success_url = reverse_lazy(
+        'homepage')  # Specifică URL-ul la care utilizatorul va fi redirecționat după ce a făcut cu succes o rezervare.
 
 
 def rezervare(request):
+    '''
+    Primește un obiect request, care conține datele cererii HTTP. Funcția obține toate serviciile din baza de date și
+    le afișează utilizând șablonul 'services/rezervare.html'.
+    '''
     all_services = Service.objects.all()
     return render(request, 'services/rezervare.html', {'all_services': all_services})
 
 
 def rezerva_servicii(request):
-    if request.POST and request.user.is_authenticated:
-        current_user_id = request.user.id
+    if request.POST and request.user.is_authenticated:  # verificam daca cererea este de timp POSt ai utilizatorul este autentificat
+        current_user_id = request.user.id  # preluam id-ul userului autentificat
         services_ids = [int(item) for item in dict(request.POST) if
-                        item != 'csrfmiddlewaretoken' and item != 'data_ora']
+                        item != 'csrfmiddlewaretoken' and item != 'data_ora']  # obtinem id-urile serviciilor selectate
         date = request.POST.get('data_ora')
-        for service_id in services_ids:
-            service = Service.objects.get(id=service_id)
+        for service_id in services_ids:  # parcurgem id-urile serviciilor selectate
+            service = Service.objects.get(
+                id=service_id)  # obținem obiectele Service asociate din baza de date folosind interogări către modelul Service
+            ''' pentru fiecare id de serviciu, se creează un obiect RezervareServicii cu informațiile despre serviciu,
+             utilizatorul curent și data rezervării. Acest obiect este apoi salvat în baza de date.
+            '''
             serviciu = RezervareServicii(
                 id_service=service,
                 id_user=current_user_id,
                 date=date
             )
             serviciu.save()
-        all_employees = Employee.objects.all()
-        return render(request, 'services/rezervare_programare.html', {'employees': all_employees})
+        return render(request, 'services/rezervare_programare.html')
     return HttpResponseRedirect(reverse("login"))
 
 
 class RezervareProgramareView(CreateView):
     template_name = 'services/rezervare_programare.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['employees'] = Employee.objects.all()
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['employees'] = Employee.objects.all()
+    #     return context
 
 
 # def selecteaza_data_ora(request):
@@ -113,10 +117,10 @@ class RezervareProgramareView(CreateView):
 
 
 def selecteaza_data_ora(request):
-    if not request.user.is_authenticated:
+    if not request.user.is_authenticated:  # daca userul nu este autentificat, il redirectionam pe login
         return HttpResponseRedirect(reverse("login"))
 
-    if request.POST:
+    if request.POST:  # daca cererea este de tip POST se obtin datele din formular
         form = SelectareDataOraForm(request.POST)
         if form.is_valid():
             date = form.cleaned_data.get('data_ora')
